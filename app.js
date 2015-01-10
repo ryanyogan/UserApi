c36049da6837df3400ff84bcd9c1e8a4ce30921c
Ryan Yogan <ryanyogan@gmail.com>
2015-01-10 16:23:04 -0600

    Added ability to retreive a user by id

--- app.js
+++ app.js
@@ -9,16 +9,30 @@ var db = monk('localhost/apiUsers');
 var users = wrap(db.get('users'));
 
 app.use(routes.post('/user', addUser));
+app.use(routes.get('/user/:id', getUser));
 
 app.listen(3000);
 console.log("The app is running on port 3000");
 
 function *addUser() {
   var userFromRequest = yield parse(this);
+
+  if (!userFromRequest.name) {
+    this.throw(400, "name required");
+  }
+
   var insertedUser = yield users.insert(userFromRequest);
   this.set("location", "/user/" + insertedUser._id);
   this.status = 200;
 }
 
+function *getUser(id) {
+  var user = yield users.findById(id);
+
+  this.body = user;
+  this.status = 200;
+}
+
 
 module.exports = app;
+module.exports.users = users;
--- test.js
+++ test.js
@@ -1,8 +1,14 @@
 var app = require('./app');
 var request = require('supertest').agent(app.listen());
+var co = require('co');
 
 describe("Simple user HTTP Crud API", function() {
-  var aUser = { name: "Ryan", age: 29, height: 1.95 };
+  var aUser = {};
+
+  beforeEach(function(done) {
+    aUser = { name: "Ryan", age: 29, height: 1.95 };
+    done();
+  });
 
   it("adds new users", function(done) {
     request
@@ -10,5 +16,30 @@ describe("Simple user HTTP Crud API", function() {
       .send(aUser)
       .expect("location", /^\/user\/[0-9a-fA-F]{24}$/)
       .expect(200, done)
+  });
+
+  it("fails with validation error for users without a name", function(done) {
+    delete aUser.name;
+
+    request
+      .post("/user")
+      .send(aUser)
+      .expect("name required")
+      .expect(400, done);
+  });
+
+  it("gets existing user by id", function(done) {
+    co(function *() {
+      var insertedUser = yield app.users.insert(aUser);
+      var url = "/user/" + insertedUser._id;
+
+      request
+        .get(url)
+        .set("Accept", "application/json")
+        .expect("Content-type", /json/)
+        .expect(/Ryan/)
+        .expect(/29/)
+        .expect(200, done);
+    });
   });
 });
